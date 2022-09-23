bde110d0e22aa2d52fc6266e5a134e9a1fab72df
breynergiral <bsgiral@misena.edu.co>
2022-09-23 09:36:54 -0500

    evaluacion

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BootcampController;
+use App\Http\Controllers\CoursesController;
+use App\Http\Controllers\ReviewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +25,5 @@ use App\Http\Controllers\BootcampController;
 //Metodo apiresourse: permite crear rutas de cambio para una entidad
 
 Route::apiResource('bootcamps', BootcampController::class);
+Route::apiResource('courses', CoursesController::class);
+Route::apiResource('reviews', ReviewsController::class);
